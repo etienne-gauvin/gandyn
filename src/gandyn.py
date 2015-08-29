170b2d3ada69b520c77380641d8720b7073473e4
Jean Blanchard <jean@blanchard.io>
2015-08-29 16:27:25 +0200

    Generic ip retriever

--- src/gandyn.py
+++ src/gandyn.py
@@ -12,12 +12,15 @@ API_KEY = ''
 DOMAIN_NAME = 'mydomain.com'
 TTL = 300
 
-RECORD = {'type':'A', 'name':'@'}
+RECORD = {'type': 'A', 'name': '@'}
 
 LOG_LEVEL = logging.INFO
 LOG_FILE = 'gandyn.log'
 
-class GandiDomainUpdater( object ):
+URL_PAGE = 'http://www.trackip.net/ip'
+
+
+class GandiDomainUpdater(object):
   """Updates a gandi DNS record value."""
   def __init__(self, api_key, domain_name, record):
     """Constructor
@@ -33,26 +36,26 @@ class GandiDomainUpdater( object ):
     self.__api = xmlrpc.client.ServerProxy('https://rpc.gandi.net/xmlrpc/')
     self.__zone_id = None
 
-  def __get_active_zone_id( self ):
+  def __get_active_zone_id(self):
     """Retrieve the domain active zone id."""
-    if self.__zone_id == None :
+    if self.__zone_id is None:
       self.__zone_id = self.__api.domain.info(
-        self.api_key,
-        self.domain_name
-        )['zone_id']
+          self.api_key,
+          self.domain_name
+      )['zone_id']
     return self.__zone_id
 
-  def get_record_value( self ):
+  def get_record_value(self):
     """Retrieve current value for the record to update."""
     zone_id = self.__get_active_zone_id()
     return self.__api.domain.zone.record.list(
-      self.api_key,
-      zone_id,
-      0,
-      self.record
-      )[0]['value']
+        self.api_key,
+        zone_id,
+        0,
+        self.record
+    )[0]['value']
 
-  def update_record_value( self, new_value, ttl=300 ):
+  def update_record_value(self, new_value, ttl=300):
     """Updates record value.
 
     Update is done on a new zone version. If an error occurs,
@@ -62,56 +65,58 @@ class GandiDomainUpdater( object ):
     new_zone_version = None
     zone_id = self.__get_active_zone_id()
     try:
-      #create new zone version
+      # create new zone version
       new_zone_version = self.__api.domain.zone.version.new(
-        self.api_key,
-        zone_id
-        )
+          self.api_key,
+          zone_id
+      )
       logging.debug('DNS working on a new zone (version %s)', new_zone_version)
       record_list = self.__api.domain.zone.record.list(
-        self.api_key,
-        zone_id,
-        new_zone_version,
-        self.record
-        )
-      #Update each record that matches the filter
+          self.api_key,
+          zone_id,
+          new_zone_version,
+          self.record
+      )
+      # Update each record that matches the filter
       for a_record in record_list:
-       #get record id
+       # get record id
        a_record_id = a_record['id']
        a_record_name = a_record['name']
        a_record_type = a_record['type']
 
-       #update record value
+       # update record value
        new_record = self.record.copy()
        new_record.update({'name': a_record_name, 'type': a_record_type, 'value': new_value, 'ttl': ttl})
-       updated_record = self.__api.domain.zone.record.update(
-         self.api_key,
-         zone_id,
-         new_zone_version,
-         {'id': a_record_id},
-         new_record
-         )
-    except xmlrpc.client.Fault as e:
-      #delete updated zone
-      if new_zone_version != None :
+       self.__api.domain.zone.record.update(
+           self.api_key,
+           zone_id,
+           new_zone_version,
+           {'id': a_record_id},
+           new_record
+       )
+    except xmlrpc.client.Fault:
+      # delete updated zone
+      if new_zone_version is not None:
         self.__api.domain.zone.version.delete(
+            self.api_key,
+            zone_id,
+            new_zone_version
+        )
+      raise
+    else:
+      # activate updated zone
+      self.__api.domain.zone.version.set(
           self.api_key,
           zone_id,
           new_zone_version
-          )
-      raise
-    else:
-      #activate updated zone
-      self.__api.domain.zone.version.set(
-        self.api_key,
-        zone_id,
-        new_zone_version
-        )
+      )
+
 
 def usage(argv):
-  print(argv[0],' [[-c | --config] <config file>] [-h | --help]')
+  print(argv[0], ' [[-c | --config] <config file>] [-h | --help]')
   print('\t-c --config <config file> : Path to the config file')
   print('\t-h --help                 : Displays this text')
+
 
 def main(argv, global_vars, local_vars):
   try:
@@ -119,7 +124,7 @@ def main(argv, global_vars, local_vars):
     for opt, arg in options:
       if opt in ('-c', '--config'):
         config_file = arg
-        #load config file
+        # load config file
         exec(
             compile(open(config_file).read(), config_file, 'exec'),
             global_vars,
@@ -134,21 +139,25 @@ def main(argv, global_vars, local_vars):
     exit(1)
 
   try:
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', level=LOG_LEVEL, filename=LOG_FILE)
-    public_ip_retriever = ipretriever.adapter.IPEcho()
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%a, %d %b %Y %H:%M:%S',
+        level=LOG_LEVEL,
+        filename=LOG_FILE)
+    public_ip_retriever = ipretriever.adapter.Generic(URL_PAGE)
     gandi_updater = GandiDomainUpdater(API_KEY, DOMAIN_NAME, RECORD)
 
-    #get DNS record ip address
+    # get DNS record ip address
     previous_ip_address = gandi_updater.get_record_value()
     logging.debug('DNS record IP address : %s', previous_ip_address)
 
-    #get current ip address
+    # get current ip address
     current_ip_address = public_ip_retriever.get_public_ip()
     logging.debug('Current public IP address : %s', current_ip_address)
 
     if current_ip_address != previous_ip_address:
-      #update record value
-      gandi_updater.update_record_value( current_ip_address, TTL )
+      # update record value
+      gandi_updater.update_record_value(current_ip_address, TTL)
       logging.info('DNS updated')
     else:
       logging.debug('Public IP address unchanged. Nothing to do.')
--- src/ipretriever/adapter.py
+++ src/ipretriever/adapter.py
@@ -5,42 +5,33 @@ import urllib.request
 import re
 import ipretriever
 
-class IfConfig( object ):
-  def get_public_ip( self ):
+class Generic(object):
+  def __init__(self, url_page):
+    self.url_page = url_page
+
+  def get_public_ip(self):
     """Returns the current public IP address. Raises an exception if an issue occurs."""
     try:
-      url_page = 'http://ifconfig.me/ip'
-      public_ip = None
-
-      f = urllib.request.urlopen(url_page)
+      f = urllib.request.urlopen(self.url_page)
       data = f.read().decode("utf8")
       f.close()
       pattern = re.compile('\d+\.\d+\.\d+\.\d+')
       result = pattern.search(data, 0)
-      if result == None:
-        raise ipretriever.Fault('Service '+url_page+' failed to return the current public IP address')
+      if result is None:
+        raise ipretriever.Fault('Service ' + self.url_page + ' failed to return the current public IP address')
       else:
-        public_ip = result.group(0)
+        return result.group(0)
     except urllib.error.URLError as e:
       raise ipretriever.Fault(e)
-    return public_ip
-    
-class IPEcho( object ):
-  def get_public_ip( self ):
-    """Returns the current public IP address. Raises an exception if an issue occurs."""
-    try:
-      url_page = 'http://ipecho.net/plain'
-      public_ip = None
 
-      f = urllib.request.urlopen(url_page)
-      data = f.read().decode("utf8")
-      f.close()
-      pattern = re.compile('\d+\.\d+\.\d+\.\d+')
-      result = pattern.search(data, 0)
-      if result == None:
-        raise ipretriever.Fault('Service '+url_page+' failed to return the current public IP address')
-      else:
-        public_ip = result.group(0)
-    except urllib.error.URLError as e:
-      raise ipretriever.Fault(e)
-    return public_ip
+class IfConfig(Generic):
+  def __init__(self):
+    super(IfConfig, self).__init__('http://ifconfig.me/ip')
+
+class IPEcho(Generic):
+  def __init__(self):
+    super(IPEcho, self).__init__('http://ipecho.net/plain')
+
+class TrackIP(Generic):
+  def __init__(self):
+    super(TrackIP, self).__init__('http://www.trackip.net/ip')
