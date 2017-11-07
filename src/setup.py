6724612b11983e75cf295f1117937410215a1472
fa18swiss <fa18swiss@users.noreply.github.com>
2017-11-07 20:01:31 +0100

    Version 0.2.1

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='gandyn',
-      version='0.2',
+      version='0.2.1',
       description='Dynamic record update client for the Gandy LiveDNS API.',
       author='Charly Caulet',
       author_email='charly.caulet@no-log.org',
